dc5dc84c2687e4dad7eccb90a182fcc8d1511902
Vishal Singh <vishalsingh092001@gmail.com>
2022-01-17 18:06:44 +0530

    updated

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mongoURI :
-    'mongodb+srv://mongoUser:GRxuUZ6EV17lG7Ff@cluster0.fews7.mongodb.net/mernstack?retryWrites=true&w=majority',
+    'Git Guardian Said Not to include Mongo URI',
     secretOrPrivateKey :
     'WhatIsThisAllAbout'
 }
